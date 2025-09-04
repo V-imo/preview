e65fae6bc0a2ae26a24a541e28e8b193c4f69f61
Alf <alfgoto@gmail.com>
2025-09-04 18:41:55 +0200

    fix: use text in string

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "V&apos;imo",
-  description: "The V&apos;imo app is the best way to manage your inspections",
+  title: "V'imo",
+  description: "The V'imo app is the best way to manage your inspections",
 }
 
 export default function RootLayout({
